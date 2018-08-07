7f1b5f16e489e6a1fc875f018047e60b1dfaa5f8
FangYang970206 <15270989505@163.com>
2018-08-07 17:09:19 +0800

    add WGAN

--- WGAN/main.py
+++ WGAN/main.py
@@ -1,27 +1,26 @@
 import torch 
 import torch.nn as nn 
-from torch.optim import Adam
+from torch.optim import RMSprop
 from torchvision.utils import make_grid
 from model import Generate,Discriminator,weight_init
 from AnimeDataset import AnimeDataset 
-import matplotlib.pyplot as plt
 import numpy as np
 import scipy.misc
 import os, argparse
 from tqdm import tqdm
-from utils import creat_gif, visualize_loss
+from utils import creat_gif
 
 def main():
 
     parse = argparse.ArgumentParser()
 
-    parse.add_argument("--lr", type=float, default=0.0001, 
+    parse.add_argument("--lr", type=float, default=0.00005, 
                         help="learning rate of generate and discriminator")
-    parse.add_argument("--beta1", type=float, default=0.5,
-                        help="adam optimizer parameter")
-    parse.add_argument("--batch_size", type=int, default=64,
+    parse.add_argument("--clamp", type=float, default=0.01, 
+                        help="clamp discriminator parameters")
+    parse.add_argument("--batch_size", type=int, default=10,
                         help="number of dataset in every train or test iteration")
-    parse.add_argument("--dataset", type=str, default="anime",
+    parse.add_argument("--dataset", type=str, default="faces",
                         help="base path for dataset")
     parse.add_argument("--epochs", type=int, default=500,
                         help="number of training epochs")
@@ -31,11 +30,6 @@ def main():
                         help="number of training data")
 
     args = parse.parse_args()
-
-    if torch.cuda.is_available():
-        device = torch.device("cuda")
-    else:
-        device = torch.device("cpu")
 
     if not os.path.exists("saved"):
         os.mkdir("saved")
@@ -49,9 +43,11 @@ def main():
         https://drive.google.com/drive/folders/1mCsY5LEsgCnc0Txv0rpAUhKVPWVkbw5I \n ")
         exit()
 
-    # generate = torch.load("saved/generate.t7").to(device)
-    # discriminator = torch.load("saved/discriminator.t7").to(device)
-    
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+    else:
+        device = torch.device("cpu")
+
     generate = Generate().to(device)
     discriminator = Discriminator().to(device)
 
@@ -59,12 +55,10 @@ def main():
     discriminator.apply(weight_init)
 
     dataset = AnimeDataset(os.getcwd(), args.dataset, args.size_per_dataset)
-    dataload = torch.utils.data.DataLoader(dataset, batch_size=args.batch_size, shuffle=True, num_workers=4)
+    dataload = torch.utils.data.DataLoader(dataset, batch_size=args.batch_size, shuffle=True, num_workers=args.loaders)
 
-    criterion = nn.BCELoss().to(device)
-
-    optimizer_G = Adam(generate.parameters(), lr=args.lr, betas=(args.beta1, 0.999))
-    optimizer_D = Adam(discriminator.parameters(), lr=args.lr, betas=(args.beta1, 0.999))
+    optimizer_G = RMSprop(generate.parameters(), lr=args.lr)
+    optimizer_D = RMSprop(discriminator.parameters(), lr=args.lr)
 
     fixed_noise = torch.randn(64, 100, 1, 1).to(device)
 
@@ -72,50 +66,34 @@ def main():
 
         print("Main epoch{}:".format(epoch))
         progress = tqdm(total=len(dataload.dataset))
-        loss_d, loss_g = 0, 0
         
         for i, inp in enumerate(dataload):
+
             # train discriminator   
             real_data = inp.float().to(device)
-            real_label = torch.ones(inp.size()[0]).to(device)
             noise = torch.randn(inp.size()[0], 100, 1, 1).to(device)
             fake_data = generate(noise)
-            fake_label = torch.zeros(fake_data.size()[0]).to(device)
             optimizer_D.zero_grad()
-            real_output = discriminator(real_data)
-            real_loss = criterion(real_output.squeeze(), real_label)
-            real_loss.backward()
-            fake_output = discriminator(fake_data)
-            fake_loss = criterion(fake_output.squeeze(), fake_label)
-            fake_loss.backward()
-            loss_D = real_loss + fake_loss
+            real_output = torch.mean(discriminator(real_data).squeeze())
+            fake_output = torch.mean(discriminator(fake_data).squeeze())
+            output = (real_output - fake_output)* -1
+            output.backward()
             optimizer_D.step()
+            
+            for param in discriminator.parameters():
+                param.data.clamp_(-args.clamp, args.clamp)
 
             #train generate
-            optimizer_G.zero_grad()
-            fake_data = generate(noise)
-            fake_label = torch.ones(fake_data.size()[0]).to(device)
-            fake_output = discriminator(fake_data)
-            loss_G = criterion(fake_output.squeeze(), fake_label)
-            loss_G.backward()
-            optimizer_G.step()
-
+            if epoch%5 == 0:
+                optimizer_G.zero_grad()
+                fake_data = generate(noise)
+                fake_output = -torch.mean(discriminator(fake_data).squeeze())
+                fake_output.backward()
+                optimizer_G.step()
+            
             progress.update(dataload.batch_size)
-            progress.set_description("D:{}, G:{}".format(loss_D.item(), loss_G.item()))
 
-            loss_g += loss_G.item()
-            loss_d += loss_D.item()
-        
-        loss_g /= (i+1)
-        loss_d /= (i+1)
-
-        with open("generate_loss.txt", 'a+') as f:
-            f.write("loss_G:{} \n".format(loss_G.item()))
-
-        with open("discriminator_loss.txt", 'a+') as f:
-            f.write("loss_D:{} \n".format(loss_D.item()))
-
-        if (epoch+1) % 20 == 0:
+        if epoch % 20 == 0:
 
             torch.save(generate, os.path.join(os.getcwd(), "saved/generate.t7"))
             torch.save(discriminator, os.path.join(os.getcwd(), "saved/discriminator.t7"))
@@ -133,11 +111,8 @@ def main():
 
     creat_gif("evolution.gif", os.path.join(os.getcwd(),"saved/img"))
 
-    visualize_loss("generate_loss.txt", "discriminator_loss.txt")
 
                 
-
-
 if __name__ == "__main__":
     main()
     
